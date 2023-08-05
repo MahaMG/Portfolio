e085b145ab081f3b2bde0622bc6a6a170275eb1d
“MahaMG” <dev.mahamg@gmail.com>
2023-08-05 11:13:18 +0300

    Modified the sidebar navigation part

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
 
-function menuToggle(){
-    const menuToggle = document.querySelector('.menuToggle');
-    const sidebar = document.querySelector(".sidebar");
-    menuToggle.classList.toggle("active");
-    sidebar.classList.toggle("active");
+function menuToggle() {
+  const menuToggle = document.querySelector(".menuToggle");
+  const sidebar = document.querySelector(".sidebar");
+  menuToggle.classList.toggle("active");
+  sidebar.classList.toggle("active");
+
+  document.querySelector(".nav-links").addEventListener("click", function (e) {
+    e.preventDefault();
+
+    // Matching strategy
+    if (e.target.classList.contains("nav-link")) {
+      const id = e.target.getAttribute("href");
+      document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+      menuToggle.classList.remove("active");
+      sidebar.classList.remove("active");
+    }
+  });
 }
 
 // ********************************
