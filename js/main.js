12cc16c5a272769f7ad06f08aaa204cafb29779f
“MahaMG” <dev.mahamg@gmail.com>
2023-11-28 19:57:32 +0300

    Updating...

--- js/main.js
+++ js/main.js
@@ -20,6 +20,19 @@ function menuToggle() {
 
 // ********************************
 
+document.querySelector(".cards").onmousemove = e => {
+  for (const card of document.querySelectorAll(".card")) {
+    const rect = card.getBoundingClientRect(),
+      x = e.clientX - rect.left,
+      y = e.clientY - rect.top;
+
+    card.style.setProperty("--mouse-x", `${x}px`);
+    card.style.setProperty("--mouse-y", `${y}px`);
+  }
+}
+
+// ********************************
+
 const toTop = document.querySelector(".top-btn");
 
 toTop.addEventListener("click", function () {
